87356d6471f25d7d90c37e4df1043b361f7f72cc
crucial-sub <jssub940@gmail.com>
2022-02-25 04:22:16 +0900

    Add: App.tsx에 라우터 설정 및 api 데이터 받아오기

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,21 @@ import DetailPage from "pages/DetailPage";
 import LinkPage from "pages/LinkPage";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "styles/GlobalStyle";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import useAxios from "hooks/useAxios";
 
 function App() {
+  const linkData = useAxios();
   return (
-    <>
+    <Container>
       <GlobalStyle />
-      <Container>
-        <LinkPage />
-        <DetailPage />
-      </Container>
-    </>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LinkPage />} />
+          <Route path="/:key" element={<DetailPage linkData={linkData} />} />
+        </Routes>
+      </Router>
+    </Container>
   );
 }
 
--- src/pages/DetailPage/index.tsx
+++ src/pages/DetailPage/index.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import type { FC } from "react";
 import DetailHeader from "components/domain/Detail/DetailHeader";
 import DetailArticle from "components/domain/Detail/DetailArticle";
+import { useParams } from "react-router-dom";
+import { linkDataType } from "utils/types";
 
-const DetailPage: FC = () => {
+const DetailPage: FC<{ linkData: linkDataType[] | null }> = ({ linkData }) => {
+  const { key } = useParams();
+  const [file, setFile] = useState<linkDataType>();
+
+  useEffect(() => {
+    if (linkData) {
+      const selectedFile = linkData.find((file) => file.key === key);
+      setFile(selectedFile);
+    }
+  }, [linkData]);
+
   return (
     <>
       <DetailHeader />
