87c09f4ac9fac0fa0c6ec57a225cf2a1cfa0af15
crucial-sub <jssub940@gmail.com>
2022-02-25 22:54:03 +0900

    Add: 만료된 링크를 다운받으려 할 시 기존과 다른 alert 표시

--- src/components/domain/Detail/DetailHeader.tsx
+++ src/components/domain/Detail/DetailHeader.tsx
@@ -1,14 +1,18 @@
 import { FC } from "react";
+import { getExpires } from "utils";
 import * as S from "./styles";
 interface DetailHeaderProps {
   linkTitle: string;
+  expires_at: number;
 }
 
-const DetailHeader: FC<DetailHeaderProps> = ({ linkTitle }) => {
+const DetailHeader: FC<DetailHeaderProps> = ({ linkTitle, expires_at }) => {
   const fileUrl = window.location.href.slice(7);
+  const expiresAt = getExpires(expires_at);
 
   const popUpAlert = () => {
-    alert("다운로드 되었습니다.");
+    if (expiresAt === "만료됨") return alert("만료된 링크입니다.");
+    else return alert("다운로드 되었습니다.");
   };
 
   return (
