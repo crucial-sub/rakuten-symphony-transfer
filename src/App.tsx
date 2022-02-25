f0826f18d2d39bcc6a211fb3276c8f8fdfc754da
ttaerrim <ltr0121@naver.com>
2022-02-25 23:43:20 +0900

    Modify: 프록시 수정

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { LinkDataType } from "types/linkTypes";
 
 function App() {
-  const linkData = useAxios<LinkDataType[] | null>(
-    "https://storage-fe.fastraffic.io/homeworks/links"
-  );
+  const linkData = useAxios<LinkDataType[] | null>("/homeworks/links");
   if (!linkData) return null;
   return (
     <Container>
--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -3,19 +3,11 @@ import { useState, useEffect } from "react";
 
 const useAxios = <T>(url: string) => {
   const [linkData, setLinkData] = useState<T>();
-
+  const PROXY = "https://storage-fe.fastraffic.io";
   useEffect(() => {
     (async () => {
       try {
-        const { data } = await axios.get(url, {
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods":
-              "GET, POST, PATCH, PUT, DELETE, OPTIONS",
-            "Access-Control-Allow-Headers":
-              "Origin, Content-Type, X-Auth-Token",
-          },
-        });
+        const { data } = await axios.get(`${PROXY}${url}`);
         setLinkData(data);
       } catch (error) {
         console.log(error);
