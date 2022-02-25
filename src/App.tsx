f29e9dfae5c5de88373e601f369e6761db86771d
Ubermensch0608 <melancholyee28@gmail.com>
2022-02-25 09:43:14 +0900

    Add: Router설정 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import React from "react";
 import Container from "components/layout/Container";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import DetailPage from "pages/DetailPage";
 import LinkPage from "pages/LinkPage";
-import { ThemeProvider } from "styled-components";
+
 import GlobalStyle from "styles/GlobalStyle";
-import { useEffect } from "react";
-import useAxios from "hooks/useAxios";
 
 function App() {
   return (
     <>
-      <GlobalStyle />
       <Container>
-        <LinkPage />
-        {/* <DetailPage /> */}
+        <GlobalStyle />
+        <Router>
+          <Routes>
+            <Route path="/" element={<LinkPage />} />
+            <Route path="/:key" element={<DetailPage />} />
+          </Routes>
+        </Router>
       </Container>
     </>
   );
