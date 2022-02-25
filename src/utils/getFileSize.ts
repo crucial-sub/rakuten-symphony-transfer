88bac07c60609312cde8d97962ed67868a8deec0
crucial-sub <jssub940@gmail.com>
2022-02-25 20:05:16 +0900

    Fix: 파일 사이즈 포맷 함수 수정

--- src/utils/getFileSize.ts
+++ src/utils/getFileSize.ts
@@ -1,12 +1,31 @@
-export const getFileSize = (fileSize: number) => {
-  const b = fileSize;
-  const kb = (Number(b) / 1000).toFixed(2);
-  const mb = (Number(kb) / 1000).toFixed(2);
-  const gb = (Number(mb) / 1000).toFixed(2);
-  const tb = (Number(gb) / 1000).toFixed(2);
-  if (Number(tb) >= 1) return `${tb}TB`;
-  else if (Number(gb) >= 1) return `${gb}GB`;
-  else if (Number(mb) >= 1) return `${mb}MB`;
-  else if (Number(kb) >= 1) return `${kb}KB`;
-  else return `${b}B`;
+export const getFileSize = (size: number) => {
+  let times = 0;
+  let bytes: string = "B";
+  while (size > 1024) {
+    size = size / 1024;
+    times++;
+  }
+  bytes = getBytes(times);
+  return `${size.toFixed(2)}${bytes}`;
+};
+
+const getBytes = (times: number) => {
+  switch (times) {
+    case 1:
+      return "KB";
+      break;
+    case 2:
+      return "MB";
+      break;
+    case 3:
+      return "GB";
+      break;
+    case 4:
+      return "TB";
+      break;
+    default:
+    case 0:
+      return "B";
+      break;
+  }
 };
